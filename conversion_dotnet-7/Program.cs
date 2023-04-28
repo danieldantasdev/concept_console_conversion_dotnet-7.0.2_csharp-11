ede9a35880277923f04a1639890a618a2233c4d0
lobinhodev <contatolobinhodev@gmail.com>
2023-04-28 16:15:47 -0300

    init: commit initial

---
+++ conversion_dotnet-7/Program.cs
@@ -0,0 +1,28 @@
+﻿//conversion implicit
+
+var noteStudent = 10;
+double noteStudentDoubleConversionImplicit = noteStudent;
+
+//conversion explicit
+
+int noteStudentDoubleConversionExplicit = (int)noteStudentDoubleConversionImplicit;
+
+//Convert
+
+string noteString = "10j";
+int noteConvert = Convert.ToInt32(noteString);
+
+//Parse
+
+int noteParse = int.Parse(noteString);
+
+if (int.TryParse(noteString, out int noteTryParse))
+{
+    Console.Write(noteTryParse);
+}
+else
+{
+    Console.Write("Número em fórmato inválido");
+}
+
+// Console.Write(noteParse);
